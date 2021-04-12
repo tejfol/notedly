08f6237a9d3cb4e8c9d8411e8418440af833cc56
tejfol <tejfolcss@gmail.com>
2021-04-12 18:05:13 +0300

    Apollo Aded

--- src/index.js
+++ src/index.js
@@ -1,8 +1,61 @@
 import express from 'express';
 
-const app = express();
+import { ApolloServer, gql } from 'apollo-server-express';
+
 const port = process.env.PORT || 4000;
+
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
+//Apollo scheme
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+  type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello WoRld!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
+  }
+};
+
+const app = express();
+
+//Apollo server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/api' });
 
 app.get('/', (req, res) => res.send('Hellow World!'));
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, () =>
+  console.log(`listening on port ${port}${server.graphqlPath}`)
+);
