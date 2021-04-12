e1d807ded8652e2fbfeacca922c2cf321a2238dc
tejfol <tejfolcss@gmail.com>
2021-04-12 07:42:50 +0300

    Initialized

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-// index.js
-// This is the main entry point of our application
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => res.send('Hellow World!'));
+
+app.listen(port, () => console.log(`listening on port ${port}`));
