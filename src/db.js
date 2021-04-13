88919a2ec0a3bd84029aae732c2a322cff462d50
tejfol <tejfolcss@gmail.com>
2021-04-13 08:33:00 +0300

    Added mongodb.

---
+++ src/db.js
@@ -0,0 +1,21 @@
+import mongoose, { mongo } from 'mongoose';
+
+module.exports = {
+  connect: DB_HOST => {
+    mongoose.set('useNewUrlParser', true);
+    mongoose.set('useFindAndModify', false);
+    mongoose.set('useCreateIndex', true);
+    mongoose.set('useUnifiedTopology', true);
+    mongoose.connect(DB_HOST);
+    mongoose.connection.on('error', err => {
+      console.log(err);
+      console.log(
+        'MongoDB connection error, Please make sure MongoDB is running.'
+      );
+      process.exit();
+    });
+    close: () => {
+      mongoose.connection.close();
+    };
+  }
+};
