7591dc8c81402d7d6924110a929fd0b6b8d086b5
tejfol <tejfolcss@gmail.com>
2021-04-15 08:41:40 +0300

    make it better

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,8 +1,8 @@
-import {Query} from './query.js';
-import {Mutation} from './mutation.js';
+import { Query } from './query.js';
+import { Mutation } from './mutation.js';
 import pkg from 'graphql-iso-date';
-import {Note} from './note.js';
-import {User} from './user.js';
+import { Note } from './note.js';
+import { User } from './user.js';
 const { GraphQLDateTime } = pkg;
 
 export const resolvers = {
